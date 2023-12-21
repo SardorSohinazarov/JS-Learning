44d0d9935581af6b8cbac67f26a8c41b7e781ac4
SardorSohinazarov <sardorstudent0618gmail.com>
2023-12-22 02:37:23 +0500

    JS uncha muncha

---
+++ Lesson2/script.js
@@ -0,0 +1,55 @@
+//alert
+//alert("Qales");
+
+//confirmation
+// var cn = confirm("Qales");
+// console.log(cn);
+
+//promp
+// const holat = prompt('Qales','');
+// console.log(holat);
+
+//list promp
+// const favouriteColor = [];
+// favouriteColor[0] = prompt("Yoqtirgan rangingiz",""); 
+// favouriteColor[1] = prompt("Yana Yoqtirgan rangingiz",""); 
+// favouriteColor[2] = prompt("Yana Yoqtirgan rangingiz","");
+
+// console.log(favouriteColor);
+
+//interpolation
+// var channel = prompt("Youtube Channal","");
+// console.log(`https://youtube.com/@${channel}/videos`)
+
+//uygavazifa
+// let numberofSerias = prompt("Nechta o'yin ko'rdingiz","");
+// console.log(numberofSerias);
+
+//uygavazifa2
+// let count = prompt('Count');
+// let b = false;
+
+// let seriesDB = {
+//     count:count,
+//     series:{},
+//     actors:{},
+//     genres:[],
+//     privat:b,
+// }
+
+// console.log(seriesDB);
+
+var i =0;
+while(i < 100){
+    i ++;
+    console.log(i);
+}
+
+do{
+    console.log(i);
+    i++;
+}while(i < 200)
+
+for(i; i< 300; i++){
+    console.log(i);
+}
